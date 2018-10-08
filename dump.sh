820e5fa4de2c03f25b17d68a986d9e4cc0a6a262
orvice <orvice@orx.me>
2018-10-08 13:00:16 +0800

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,42 +1,27 @@
 #!/bin/bash
 
-echo DB_HOST: $DB_HOST
-echo DB_USER: $DB_USER
+echo DIR: $DIR
+echo NAME: $NAME
 echo BUCKET: $BUCKET
-echo DB_NAME: $DB_NAME
 echo END_POINT: $END_POINT
 
 NOW=`date +%Y-%m-%d-%H-%M`
-FILENAME=$DB_NAME.$NOW.sql
+FILENAME=$NOW.tar.gz
 ERRORFILE=$DB_NAME.$NOW.error.log
 
-# --add-drop-trigger is not a known option to this mysqldump when run against a
-#   5.7.11 MySQL RDS instance.
-#
-# --opt is enabled be default which is shorthand for the following:
-#   --add-drop-table --add-locks --create-options
-#   --disable-keys --extended-insert --lock-tables --quick --set-charset
-mysqldump --all-databases --add-drop-database \
-  --compact --routines \
-  --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD  \
-  --log-error=/tmp/$ERRORFILE \
-  > /tmp/$FILENAME
+
 
 # creates $FILENAME.gz
-gzip /tmp/$FILENAME
-gzip /tmp/$ERRORFILE
+# gzip /tmp/$FILENAME
+tar -zcvf /tmp/$NOW $DIR 
 
 # ensure the bucket is present
 aws s3 mb s3://$BUCKET --region us-east-1 --endpoint-url $END_POINT
 
-aws s3 cp /tmp/$FILENAME.gz s3://$BUCKET/mysql-dump/$DB_NAME/$FILENAME.gz --endpoint-url $END_POINT
-aws s3 cp /tmp/$ERRORFILE.gz s3://$BUCKET/mysql-dump/$DB_NAME/$ERRORFILE.gz --endpoint-url $END_POINT
+aws s3 cp /tmp/$FILENAME.gz s3://$BUCKET/dir/$NAME/$FILENAME.gz --endpoint-url $END_POINT
+
 
 aws s3 ls s3://$BUCKET/mysql-dump/$DB_NAME/$FILENAME.gz --endpoint-url $END_POINT
-aws s3 ls s3://$BUCKET/mysql-dump/$DB_NAME/$ERRORFILE.gz --endpoint-url $END_POINT
+
 
 /bin/rm -f /tmp/$FILENAME.gz
-/bin/rm -f /tmp/$ERRORFILE.gz
-
-
-
